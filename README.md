9fe8f9dd3ad7a65c9535715e4950fb12522aea10
Paige Vogenthaler <paige.vogenthaler@gmail.com>
2022-10-22 10:28:14 -0500

    update readme and clean up react code

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # garmin-report
+
+A python, react, and nextjs application that creates visualizations with garmin data via [python-garminconnect](https://github.com/cyberjunky/python-garminconnect).
+
+## About
+
+This application:
+
+- Runs an hourly cron job via a github actions workflow that calls a vercel python serverless function, which fetches garmin data and pushes it to S3.
+- Uses nextjs `getServerSideProps` to call another vercel python serverless function that retrieves the current garmin data stored in S3.
+- Renders the garmin data via react components.
+
+## Environment Variables
+
+In the `.env`:
+
+```
+GARMIN_USERNAME="..."
+GARMIN_PASSWORD="..."
+
+S3_AWS_ACCESS_KEY_ID="..."
+S3_AWS_SECRET_ACCESS_KEY="..."
+```
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Calendar from "react-calendar";
+import ReactCalendar from "react-calendar";
 
 export const getServerSideProps = async () => {
   try {
@@ -14,117 +14,160 @@ export const getServerSideProps = async () => {
   }
 };
 
+const getIntensityMinutes = (garminData, date) =>
+  garminData[date]
+    ? garminData[date].moderateIntensityMinutes +
+      2 * garminData[date].vigorousIntensityMinutes
+    : null;
+
+const formatDate = (date) => date.toISOString().split("T")[0];
+
+const CalendarTile = ({ date, view, garminData }) => {
+  const intensityMinutes = getIntensityMinutes(garminData, formatDate(date));
+  const activeKilocalories = garminData[formatDate(date)]?.activeKilocalories;
+  const totalSteps = garminData[formatDate(date)]?.totalSteps;
+  const shouldShowData =
+    view === "month" &&
+    date <= new Date() &&
+    (!!intensityMinutes || !!activeKilocalories || !!totalSteps);
+
+  const Data = ({ children }) => (
+    <div
+      style={{
+        paddingLeft: "5px",
+        fontSize: "12px",
+      }}
+    >
+      {children}
+    </div>
+  );
+
+  return (
+    <div
+      style={{
+        minHeight: "70px",
+        width: "100%",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        textAlign: "left",
+        padding: "8px 0px 8px 12px",
+      }}
+    >
+      {shouldShowData && (
+        <>
+          <Data>IM: {intensityMinutes.toLocaleString()}</Data>
+          <Data>AC: {activeKilocalories.toLocaleString()}</Data>
+          <Data>S: {totalSteps.toLocaleString()}</Data>
+        </>
+      )}
+    </div>
+  );
+};
+
+const WeekData = ({ garminData, activeStartDate, view, value }) => {
+  if (view !== "month") {
+    return;
+  }
+
+  let date;
+
+  if (activeStartDate) {
+    date = new Date(activeStartDate);
+  } else {
+    date = new Date(value);
+    // set date to first of the month
+    date.setDate(1);
+  }
+
+  // set date to Monday
+  while (date.getDay() !== 1) {
+    date.setDate(date.getDate() - 1);
+  }
+
+  const weekData = [];
+
+  [...Array(6)].forEach((week) => {
+    let weekIntensityMinutes = 0;
+    let weekActiveCalories = 0;
+    let weekSteps = 0;
+    [...Array(7)].forEach((day) => {
+      weekIntensityMinutes += getIntensityMinutes(garminData, formatDate(date));
+      weekActiveCalories +=
+        garminData[formatDate(date)]?.activeKilocalories || 0;
+      weekSteps += garminData[formatDate(date)]?.totalSteps || 0;
+      date.setDate(date.getDate() + 1);
+    });
+
+    weekData.push(
+      <li
+        key={date}
+        style={{
+          flex: 1,
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-between",
+          fontSize: "12px",
+          paddingBottom: "calc(10px + 8px)",
+          paddingTop: "calc(10px + 16px + 8px)",
+        }}
+      >
+        {weekIntensityMinutes ? (
+          <>
+            <div>
+              Intensity Minutes: {weekIntensityMinutes.toLocaleString()}
+            </div>
+            <div>Active Calories: {weekActiveCalories.toLocaleString()}</div>
+            <div>Steps: {weekSteps.toLocaleString()}</div>
+          </>
+        ) : null}
+      </li>
+    );
+  });
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        flex: 1,
+        border: "1px solid #a0a096",
+        borderLeftWidth: "0px",
+        borderRadius: "0 15px 15px 0",
+      }}
+    >
+      <div
+        style={{
+          marginTop: "calc(44px + 1em)",
+          padding: "7px",
+          textTransform: "uppercase",
+          fontSize: "14px",
+          textAlign: "center",
+        }}
+      >
+        Week Total
+      </div>
+      <ul
+        style={{
+          flex: 1,
+          padding: "0 20px",
+          margin: 0,
+          listStyle: "none",
+          display: "flex",
+          flexDirection: "column",
+          minHeight: "70px",
+        }}
+      >
+        {weekData}
+      </ul>
+    </div>
+  );
+};
+
 const App = ({ garminData }) => {
   const [value, setValue] = useState(new Date());
   const [view, setView] = useState("month");
   const [activeStartDate, setActiveStartDate] = useState(null);
-
-  const getIntensityMinutes = (date) =>
-    garminData[date]
-      ? garminData[date].moderateIntensityMinutes +
-        2 * garminData[date].vigorousIntensityMinutes
-      : null;
-
-  const formatDate = (date) => date.toISOString().split("T")[0];
-
-  const WeekData = () => {
-    if (view !== "month") {
-      return;
-    }
-
-    let date;
-
-    if (activeStartDate) {
-      date = new Date(activeStartDate);
-    } else {
-      date = new Date(value);
-      // set date to first of the month
-      date.setDate(1);
-    }
-
-    // set date to Monday
-    while (date.getDay() !== 1) {
-      date.setDate(date.getDate() - 1);
-    }
-
-    const weekData = [];
-
-    [...Array(6)].forEach((week) => {
-      let weekIntensityMinutes = 0;
-      let weekActiveCalories = 0;
-      let weekSteps = 0;
-      [...Array(7)].forEach((day) => {
-        weekIntensityMinutes += getIntensityMinutes(formatDate(date));
-        weekActiveCalories +=
-          garminData[formatDate(date)]?.activeKilocalories || 0;
-        weekSteps += garminData[formatDate(date)]?.totalSteps || 0;
-        date.setDate(date.getDate() + 1);
-      });
-
-      weekData.push(
-        <li
-          key={date}
-          style={{
-            flex: 1,
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "space-between",
-            fontSize: "12px",
-            paddingBottom: "calc(10px + 8px)",
-            paddingTop: "calc(10px + 16px + 8px)",
-          }}
-        >
-          {weekIntensityMinutes ? (
-            <>
-              <div>
-                Intensity Minutes: {weekIntensityMinutes.toLocaleString()}
-              </div>
-              <div>Active Calories: {weekActiveCalories.toLocaleString()}</div>
-              <div>Steps: {weekSteps.toLocaleString()}</div>
-            </>
-          ) : null}
-        </li>
-      );
-    });
-
-    return (
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          flex: 1,
-          border: "1px solid #a0a096",
-          borderLeftWidth: "0px",
-          borderRadius: "0 15px 15px 0",
-        }}
-      >
-        <div
-          style={{
-            marginTop: "calc(44px + 1em)",
-            padding: "7px",
-            textTransform: "uppercase",
-            fontSize: "14px",
-            textAlign: "center",
-          }}
-        >
-          Week Total
-        </div>
-        <ul
-          style={{
-            flex: 1,
-            padding: "0 20px",
-            margin: 0,
-            listStyle: "none",
-            display: "flex",
-            flexDirection: "column",
-            minHeight: "70px",
-          }}
-        >
-          {weekData}
-        </ul>
-      </div>
-    );
-  };
 
   return (
     <>
@@ -138,7 +181,7 @@ const App = ({ garminData }) => {
         }}
       >
         <div style={{ display: "flex", justifyContent: "center" }}>
-          <Calendar
+          <ReactCalendar
             showFixedNumberOfWeeks
             onChange={setValue}
             value={value}
@@ -146,66 +189,11 @@ const App = ({ garminData }) => {
             onActiveStartDateChange={({
               activeStartDate: newActiveStartDate,
             }) => setActiveStartDate(newActiveStartDate)}
-            tileContent={({ date, view }) => {
-              const intensityMinutes = getIntensityMinutes(formatDate(date));
-              const activeKilocalories =
-                garminData[formatDate(date)]?.activeKilocalories;
-              const totalSteps = garminData[formatDate(date)]?.totalSteps;
-              const shouldShowData =
-                view === "month" &&
-                date <= new Date() &&
-                (!!intensityMinutes || !!activeKilocalories || !!totalSteps);
-              return (
-                <div
-                  style={{
-                    minHeight: "70px",
-                    width: "100%",
-                    display: "flex",
-                    flexDirection: "column",
-                    justifyContent: "space-between",
-                    textAlign: "left",
-                    padding: "8px 0px 8px 12px",
-                  }}
-                >
-                  {shouldShowData && (
-                    <>
-                      <div
-                        style={{
-                          // backgroundColor:
-                          //   intensityMinutes >= 30 ? "lemonchiffon" : null,
-                          paddingLeft: "5px",
-                          fontSize: "12px",
-                        }}
-                      >
-                        IM: {intensityMinutes.toLocaleString()}
-                      </div>
-                      <div
-                        style={{
-                          // backgroundColor:
-                          //   activeKilocalories >= 200 ? "peachpuff" : null,
-                          paddingLeft: "5px",
-                          fontSize: "12px",
-                        }}
-                      >
-                        AC: {activeKilocalories.toLocaleString()}
-                      </div>
-                      <div
-                        style={{
-                          // backgroundColor:
-                          //   totalSteps >= 7000 ? "lightblue" : null,
-                          paddingLeft: "5px",
-                          fontSize: "12px",
-                        }}
-                      >
-                        S: {totalSteps.toLocaleString()}
-                      </div>
-                    </>
-                  )}
-                </div>
-              );
-            }}
+            tileContent={({ date, view }) => (
+              <CalendarTile {...{ date, view, garminData }} />
+            )}
           />
-          <WeekData />
+          <WeekData {...{ garminData, activeStartDate, view, value }} />
         </div>
       </main>
       <style global jsx>{`
